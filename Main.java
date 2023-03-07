85d9c2feea33395cc8b2a6ba54aa43bcb15721a9
Replit user <tbs03171@naver.com>
2023-03-07 07:28:48 +0000

    20230307

--- Main.java
+++ Main.java
@@ -1,37 +1,36 @@
 import java.util.*;
 
 public class Main {
-
-  public static String str;
-  public static int count0 = 0; // 전부 0으로 바꾸는 경우
-  public static int count1 = 0; // 전부 1로 바꾸는 경우
-
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
 
-    // 문자열 입력 받기
-    String str = sc.next();
+    // 동전 개수 입력 받기
+    int n = sc.nextInt();
 
-    // 첫 번째 원소에 대해서 처리
-    if (str.charAt(0) == '1') {
-      count0++;
-    } else {
-      count1++;
+    // 동전 정보 입력 받기
+    int[] coins = new int[n];
+    for (int i = 0; i < n; i++) {
+      coins[i] = sc.nextInt();
     }
 
-    // 두 번째 원소부터 모든 원소를 확인하며
-    for (int i = 0; i < str.length() - 1; i++) {
-      if (str.charAt(i) != str.charAt(i + 1)) {
-        // 다음 수에서 1로 바뀌는 경우
-        if (str.charAt(i + 1) == '1')
-          count0++;
-        // 다음 수에서 0으로 바뀌는 경우
-        else
-          count1++;
+    // 동전 정보 정렬 하기
+    Arrays.sort(coins);
+
+    // 1부터 만들 수 있는지 없는지 확인
+    int result = 0;
+    boolean OK = false;
+    while (true) {
+      result++;
+      for (int i = 1; i <= result; i++) {
+        if () {
+          
+          break;
+        }
       }
+      if (OK == false) break;
     }
 
     // 결과 출력
-    System.out.println(Math.min(count0, count1));
+    System.out.println(result);
   }
 }
