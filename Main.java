63b9516229a6040ecaca7bc832275ea620d5e59e
Replit user <tbs03171@naver.com>
2023-03-13 08:20:38 +0000

    20230313

--- Main.java
+++ Main.java
@@ -1,29 +1,34 @@
 import java.util.*;
 
-public class Main {
-
-  public static int n, m;
-  // 1부터 10까지의 무게를 담을 수 있는 배열
-  public static int[] arr = new int[11];
-
-  public static void main(String[] args) {
-    Scanner sc = new Scanner(System.in);
-
-    // 볼링공의 개수 N, 공의 최대 무게 M
-    n = sc.nextInt();
-    m = sc.nextInt();
-
-    // 볼링공 무게 K 입력
-    for (int i = 0; i < n; i++) {
-      int k = sc.nextInt();
-      arr[k] += 1;
+class Solution {
+    public int solution(int[] food_times, long k) {
+        int answer = 0;
+        
+        int[] arr = food_times.clone();
+        Arrays.sort(arr);
+        
+        int cycle = 0; // 몇 싸이클 돌았는지
+        for (int i = 0; i < arr.length; i++) {
+            int remains = arr.length - i; // 남은 음식 수
+            if (k < remains) break; // 남은 음식 보다 남은 초가 더 적으면 멈추기
+            for (int j = cycle; j < arr[i]; j++) {
+                if (k < remains) { // 남은 음식 보다 남은 초가 더 적으면 멈추기
+                    break;
+                } else {
+                    k -= remains;
+                    cycle++;
+                }
+            }
+        }
+        
+        for (int i = 0; i < food_times.length; i++) {
+            if (food_times[i] < cycle) {
+                food_times[i] = 0;
+            } else {
+                food_times[i] = food_times[i] - cycle;
+            }
+        }
+        
+        return answer;
     }
-
-    //
-    int cnt = 0;
-
-    // 결과 출력
-    System.out.println(cnt);
-
-  }
 }
