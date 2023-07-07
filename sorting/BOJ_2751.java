6a8cbc4536154f52085befc0ef4b9d5dffb47529
tbs03171 <108507997+tbs03171@users.noreply.github.com>
2023-07-07 11:06:35 +0900

    [BOJ] 2751_수 정렬하기 2

---
+++ sorting/BOJ_2751.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    StringBuilder sb = new StringBuilder();
+
+    int N = sc.nextInt();
+
+    // list 계열 중 하나를 쓰면 된다.
+    ArrayList<Integer> list = new ArrayList<>();
+
+    for (int i = 0; i < N; i++) {
+      list.add(sc.nextInt());
+    }
+
+    Collections.sort(list);
+
+    for (int value : list) {
+      sb.append(value).append('\n');
+    }
+    System.out.println(sb);
+  }
+}
